b3afeefdb885f4f2852093b2707c2a77c87224b6
ASUS <ASUS@DESKTOP-MEQ9N8J>
2020-11-26 14:01:37 +0100

    Cambio2

--- ProyectoAlex/src/Alex.java
+++ ProyectoAlex/src/Alex.java
@@ -3,7 +3,8 @@ public class Alex {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hola mundo");
+System.out.println("Hola mundo alejandro");
+
 	}
 
 }
